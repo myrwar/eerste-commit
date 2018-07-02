d96f2042f0f5ebcf7f7de7608d63031c5f82c312
Myrthe Warring <myrthe@Robs-MacBook-Pro.local>
2018-07-02 11:20:04 +0200

    eerste commit

---
+++ drank.java
@@ -0,0 +1,20 @@
+class Myrthe{
+	public static void main(String[] args){
+		System.out.println("Mag iemand drinken?");
+	   int leeftijd = 25;
+           if(leeftijd <=0){
+               System.out.println("Error");
+               
+           } else if(leeftijd <= 16) {
+               System.out.println("Te jong");
+           } else if(leeftijd == 16) {
+               System.out.println("Bier of wijn");
+             } else if(leeftijd == 17) {
+               System.out.println("Bier of wijn");
+                } else if(leeftijd => 18) {
+               System.out.println("alle dranken");
+           }
+	
+	
+	}
+}
---
+++ myrthe.java
@@ -0,0 +1,12 @@
+class Myrthe{
+	public static void main(String[] args){
+		System.out.println("Mag iemand drinken");
+	
+		int a = 16;
+        int b = 18;
+        
+
+		System.out.println("Ja er mag gedronken worden");
+	
+	}
+}
